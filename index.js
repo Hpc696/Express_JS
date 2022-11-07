1fc1c10933b1575cb14261bee13bb6a5a6c8b511
Honrop696 <honropgiochi@gmail.com>
2022-11-07 14:48:59 +0100

    server semplice

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//intro mdn express
 const express = require('express')
 const app = express()
+const port = 8080
+//home di localhost:port
+app.get('/',(req,res)=>{
+    console.log("Home")
+    res.send("Home")
+})
+
+app.get('/cacca',(req,res)=>{
+    console.log("ho cacato")
+    res.send("...poop...")
+})
+//fa quello interno grande quando path = /profile
+app.get('/profile',(req,res)=>{
+    console.log('Profilo di cacca')
+    res.send('Profilo')
+})
+
+app.listen(8080,()=>{
+    console.log('express is working...')
+})
