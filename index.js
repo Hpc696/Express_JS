f8a54acdead267506939d621b13792c1dedbd2f0
Honrop696 <honropgiochi@gmail.com>
2022-11-07 15:10:13 +0100

    req.query e .params

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const port = 8080
 //home di localhost:port
 app.get('/',(req,res)=>{
     console.log("Home")
+    console.log(req.query) //stampa in console la query che inseriamo (es. '/?q=cakka)
     res.send("Home")
 })
-
-app.get('/cacca',(req,res)=>{
+//params
+app.get('/cacca/:molla/:dura',(req,res)=>{
     console.log("ho cacato")
+    console.log("Cacca molla: " ,req.params['molla'])
+    console.log("Cacca dura: " ,req.params['dura']) 
     res.send("...poop...")
 })
 //fa quello interno grande quando path = /profile
