a5ec6aff9999d644e08f30e7ca531e2c17fa4fa3
Honrop696 <honropgiochi@gmail.com>
2022-11-07 16:06:21 +0100

    intr. router

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.get('/cacca/:molla/:dura',(req,res)=>{
     res.send("...poop...")
 })
 //fa quello interno grande quando path = /profile
-app.get('/profile',(req,res)=>{
+app.get('/profile',(req,res)=>{ //al posto di req,res potrei chiamare una funzione...usando ogg. router(express.Router() --> modulo di gestione di sottopath)
     console.log('Profilo di cacca')
     console.log("Il campo nome query è: " + req.query.nome)
     res.send("Benvenuto "+req.query.nome)
-})
+}) 
 
 app.listen(8080,()=>{
     console.log('express is working...')
